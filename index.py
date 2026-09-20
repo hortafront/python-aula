cbb534c0f98744c0a0a01f873880c943b5c53028
Gustavo <guslolhorta@gmail.com>
2026-09-20 00:53:24 -0300

    colocando codigo em python

--- index.py
+++ index.py
@@ -1,12 +1,28 @@
-# Entrada de Dados 
+# Entrada de Dados
+# Declara variável
+nome = "Gustavo"
+sobrenome = "Horta"
+#String é tudo aquilo que está Entre aspas, seja simples ou duplas
+num1 = 10
+num2 = 20
 
-# Declara variavel
+# Impressão no terminal ou Mostrar no terminal
+# Comando print() é utilizado para mostrar informações na tela
 
 # Processamento de Dados
-#realimos operaçoes matematicas,
-# verificaçoes logicas
-#e manipulaçao de dados
+# Realizamos operações matemáticas, 
+# verificações lógicas 
+# e manipulação de dados
+nomeCompleto = nome + " " + sobrenome
+# Concatenação de Strings é a junção de duas ou mais Strings
+soma = num1 + num2
 
+# Saída de Dados
+# imprimir na tela o resultado de uma operação matemática
+# ou mostrar o resultado da verifição lógica
+print("Nome: " + nome)
+print("Sobrenome: " + sobrenome)
+print("O nome completo do usuário é: " + nomeCompleto)
 
 
 
